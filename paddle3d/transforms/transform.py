062e5d9f579572510e01365a966505a785a2a485
廖智民 <529641220@qq.com>
2023-02-09 13:18:46 +0800

    transform upadte

--- paddle3d/transforms/transform.py
+++ paddle3d/transforms/transform.py
@@ -17,6 +17,7 @@ from typing import Any, List, Tuple, Union
 
 import cv2
 import numpy as np
+from numpy import random
 from PIL import Image
 
 import paddle
@@ -34,7 +35,7 @@ __all__ = [
     "RandomHorizontalFlip", "RandomVerticalFlip", "GlobalRotate", "GlobalScale",
     "GlobalTranslate", "ShufflePoint", "SamplePoint", "SamplePointByVoxels",
     "FilterPointOutsideRange", "FilterBBoxOutsideRange", "HardVoxelize",
-    "RandomObjectPerturb", "ConvertBoxFormat", "ResizeShortestEdge", 'ResizeMultiImage',
+    "RandomObjectPerturb", "ConvertBoxFormat", "ResizeShortestEdge", 'ResizeMultiImage', 'PhotoMetricDistortionMultiViewImage',
     "RandomContrast", "RandomBrightness", "RandomSaturation", "ToVisionBasedBox"
 ]
 
@@ -1262,6 +1263,84 @@ class PadMultiViewImage(object):
         sample['pad_fixed_size'] = self.size
         sample['pad_size_divisor'] = self.size_divisor
         return sample
+
+@manager.TRANSFORMS.add_component
+class PhotoMetricDistortionMultiViewImage(object):
+    """Apply photometric distortion to image sequentially, every transformation
+    is applied with a probability of 0.5. The position of random contrast is in
+    second or second to last.
+    1. random brightness
+    2. random contrast (mode 0)
+    3. convert color from BGR to HSV
+    4. random saturation
+    5. random hue
+    6. convert color from HSV to BGR
+    7. random contrast (mode 1)
+    8. randomly swap channels
+    Args:
+        brightness_delta (int): delta of brightness.
+        contrast_range (tuple): range of contrast.
+        saturation_range (tuple): range of saturation.
+        hue_delta (int): delta of hue.
+    """
+
+    def __init__(self,
+                 brightness_delta=32,
+                 contrast_range=(0.5, 1.5),
+                 saturation_range=(0.5, 1.5),
+                 hue_delta=18):
+        self.brightness_delta = brightness_delta
+        self.contrast_lower, self.contrast_upper = contrast_range
+        self.saturation_lower, self.saturation_upper = saturation_range
+        self.hue_delta = hue_delta
+
+    def __call__(self, sample):
+        imgs = sample['img']
+        new_imgs = []
+        for img in imgs:
+            assert img.dtype == np.float32, \
+                'PhotoMetricDistortion needs the input image of dtype np.float32,'\
+                ' please set "to_float32=True" in "LoadImageFromFile" pipeline'
+            if random.randint(2):
+                delta = random.uniform(-self.brightness_delta,
+                                    self.brightness_delta)
+                img += delta
+            # mode == 0 --> do random contrast first
+            # mode == 1 --> do random contrast last
+            mode = random.randint(2)
+            if mode == 1:
+                if random.randint(2):
+                    alpha = random.uniform(self.contrast_lower,
+                                           self.contrast_upper)
+                    img *= alpha
+            # convert color from BGR to HSV
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+            # random saturation
+            if random.randint(2):
+                img[..., 1] *= random.uniform(self.saturation_lower,
+                                            self.saturation_upper)
+
+            # random hue
+            if random.randint(2):
+                img[..., 0] += random.uniform(-self.hue_delta, self.hue_delta)
+                img[..., 0][img[..., 0] > 360] -= 360
+                img[..., 0][img[..., 0] < 0] += 360
+
+            # convert color from HSV to BGR
+            img = cv2.cvtColor(img, cv2.COLOR_HSV2BGR)
+            # random contrast
+            if mode == 0:
+                if random.randint(2):
+                    alpha = random.uniform(self.contrast_lower,
+                                        self.contrast_upper)
+                    img *= alpha
+            # randomly swap channels
+            if random.randint(2):
+                img = img[..., random.permutation(3)]
+            new_imgs.append(img)
+        sample['img'] = new_imgs
+        return sample
+
 
 
 @manager.TRANSFORMS.add_component
